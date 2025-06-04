1313db71c80a95011025c3e4a6e26748a6155b84
Fatoumata Bintou Sylla <fatoumatbinetousylla@gmail.com>
2025-06-04 00:30:00 +0000

    Résolution des conflits

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     port: 5174,
     open: true
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        mess: resolve(__dirname, 'public/mess.html')
+      },
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': resolve(__dirname, './src')
     }
   },
   publicDir: 'public'
